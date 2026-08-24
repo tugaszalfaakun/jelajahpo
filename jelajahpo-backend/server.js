84e7c51d3383f5f503c1a4724516e3d443e70ab4
tugaszalfaakun <tugaszalfaakun@gmail.com>
2026-08-24 11:04:15 +0700

    Initial commit

---
+++ jelajahpo-backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Selamat Datang di JelajahPo API! ')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server JelajahPo jalan di http://localhost:${PORT}`);
+})
