3fd0a02294d07c80490643df856214a48acf8257
tugaszalfaakun <tugaszalfaakun@gmail.com>
2026-08-27 08:58:55 +0700

    sesi 2

--- jelajahpo-backend/server.js
+++ jelajahpo-backend/server.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const mysql = require ('mysql2');
+const PORT = 5000;
 
 app.use(express.json());
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jelajahpo_db'
+});
+
+db.connect (err => {
+    if (err) {
+        console.error('Gagal konek ke database', err);
+    } else {
+        console.log('Berhasil konek ke database JelajahPo');
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('Selamat Datang di JelajahPo API! ')
+});
+
+app.get('/wisata', (req, res) => {
+    const sql = 'SELECT * FROM wisata';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err});
+        res.json(results);
+    });
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err});
+        res.json(results);
+    });
 });
 
 app.listen(PORT, () => {
